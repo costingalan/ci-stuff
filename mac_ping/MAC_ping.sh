a234b6ce425e3661996cf61a13b51c11a0db95db
Costin Galan <galan.costin@gmail.com>
2015-08-31 17:33:15 +0300

    Repaired timezone

--- mac_ping/MAC_ping.sh
+++ mac_ping/MAC_ping.sh
@@ -3,5 +3,5 @@
 LOGFILE=$(echo "$(date +%D -u).log" | sed 's:/:-:g')
 MAC_ADDRESS=$1
 
-echo "MAC: $MAC_ADDRESS Time: $(date +%H:%M) Details: `sudo macping $MAC_ADDRESS -c 3 2>/dev/null | grep "3 packets"` " >> $LOGFILE
+echo "MAC: $MAC_ADDRESS Time: $(date +%H:%M -u) Details: `sudo macping $MAC_ADDRESS -c 3 2>/dev/null | grep "3 packets"` " >> $LOGFILE
 
