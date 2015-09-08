5fca860eb16228f2f8ab67d61ad9836ee439541c
Costin Galan <galan.costin@gmail.com>
2015-09-09 00:26:52 +0300

    Improved text formatting

--- mac_ping/MAC_pingv2.sh
+++ mac_ping/MAC_pingv2.sh
@@ -9,18 +9,18 @@ HOSTFILE=hosts.txt
 
 for line in `cat $HOSTFILE`
 do
-    MAC=`echo $line | awk -F'|' '{print $2}' `
-    HOST=`echo $line | awk -F'|' '{print $1}' `
+  MAC=`echo $line | awk -F'|' '{print $2}' `
+  HOST=`echo $line | awk -F'|' '{print $1}' `
    
-    mkdir -p $HOST/$MAC
+  mkdir -p $HOST/$MAC
 
-    [[ "$MAC" =~ "/(([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2})|(([0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}|([0-9A-Fa-f]{12}))/ig" ]] && OK=1 || OK=0
+  [[ "$MAC" =~ "/(([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2})|(([0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}|([0-9A-Fa-f]{12}))/ig" ]] && OK=1 || OK=0 #testing MAC 
 
-    if [[ OK==1 ]]
+  if [[ OK==1 ]]
     then
-        echo "Time: $(date +%H:%M -u) Details: `sudo macping $MAC -c 5 2>/dev/null | grep "5 packets"` " >> $HOST/$MAC/$LOGFILE
+      echo "Time: $(date +%H:%M -u) Details: `sudo macping $MAC -c 5 2>/dev/null | grep "5 packets"` " >> $HOST/$MAC/$LOGFILE
     else
-        echo "The MAC: $MAC is INVALID"
-    fi 
+      echo "The MAC: $MAC for HOST: $HOST is INVALID"
+  fi 
 
 done
