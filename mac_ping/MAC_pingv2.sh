25885b16cd4de5bea18869d7912ea1d015c7079c
Costin Galan <galan.costin@gmail.com>
2015-09-15 19:58:43 +0300

    Removed MAC check

--- mac_ping/MAC_pingv2.sh
+++ mac_ping/MAC_pingv2.sh
@@ -14,13 +14,6 @@ do
    
   mkdir -p "$host/$mac"
 
-  [[ "$mac" =~ /(([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2})|(([0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}|([0-9A-Fa-f]{12}))/ig ]] && ok='1' || ok='0' #testing MAC 
-
-  if [[ $ok == 1 ]]
-    then
-      echo "Time: $(date +%H:%M -u) Details: $(sudo macping "$mac" -c 5 2>/dev/null | grep "5 packets") " >> "$host/$mac/$logfile"
-    else
-      echo "The MAC: $mac for HOST: $host is INVALID"
-  fi 
+  echo "Time: $(date +%H:%M -u) Details: $(sudo macping "$mac" -c 5 2>/dev/null | grep "5 packets") " >> "$host/$mac/$logfile"
 
 done
